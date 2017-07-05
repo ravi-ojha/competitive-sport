75e4c1bc49fa8936ab1f6c02a89e7ff3fc06ceab
Ravi Ojha <raviojha2105@gmail.com>
2017-07-06 00:57:02 +0530

    Solve cf 822a

--- codeforces/822a.cpp
+++ codeforces/822a.cpp
@@ -5,13 +5,42 @@
 #define PI 3.1415926535897932384626433832795
 #define MOD 1000000007
 
+// Something to print to console and debug
+#define DEBUG false
+#define CONSOLE(x) if(DEBUG) {cout << '>' << #x << ':' << x << endl;}
+
 using namespace std;
+
+long long gcd(long long a, long long b) {
+    long long temp;
+    while(b > 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+long long factorial(long long a) {
+    long long res = 1;
+    for(long long i=1; i<=a; i++) {
+        res = res*i;
+    }
+
+    CONSOLE(res);
+    return res;
+}
 
 int main() {
     long long a, b;
     cin >> a >> b;
-    long long c;
+
+    long long c, d;
     c = min(a, b);
-    cout << c;
+
+    long long c_fact = factorial(c);
+
+    cout << c_fact << endl;
+
     return 0;
 }
